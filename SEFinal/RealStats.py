77ede6cf863545835f92869915cc5dcb97ad66fb
tking1245 <119871540+tking1245@users.noreply.github.com>
2024-09-21 00:53:29 -0400

    Update RealStats.py

--- SEFinal/RealStats.py
+++ SEFinal/RealStats.py
@@ -12,13 +12,16 @@ import yfinance
 #    writer.writerows(data)
 #print("CSV file populated successfully!")
 
-# temp code
-def create_graph(t,s): # going to add s as a functional argument in order to control the stock being studied as well
+s = input("Which stock would you like to investigate? \n please enter the stock name abreviation: ")
+t = input("How far back in time would you like to investigate?\n Please use - 1mo, 3mo, 6mo, 1y, 3y, or all: ")
 
-    stock_name = yfinance.Ticker(str(s))
-    data = stock_name.history(period = str(t))
+
+def create_graph(stock,time): # going to add s as a functional argument in order to control the stock being studied as well
+
+    stock_name = yfinance.Ticker(str(stock))
+    data = stock_name.history(period = str(time))
     df_data = pd.DataFrame(data) # convert the data to a dataFrame 
 
-    return (df_data.to_csv(str(t)+str(s)".csv" , index = True))
+    return (df_data.to_csv(str(stock)+str(time)+".csv" , index = True))
 
-create_graph(input(), input())
+create_graph(s,t)
