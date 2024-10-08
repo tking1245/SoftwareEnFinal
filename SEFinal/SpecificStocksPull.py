7dcc5dc39ee47bd40226af6e01913b1e16ffbaf8
tking1245 <119871540+tking1245@users.noreply.github.com>
2024-10-08 14:37:55 -0400

    Update SpecificStocksPull.py

--- SEFinal/SpecificStocksPull.py
+++ SEFinal/SpecificStocksPull.py
@@ -1,58 +1,20 @@
-import matplotlib
 import pandas as pd
-import csv
-from bs4 import BeautifulSoup as bsp 
-import requests
 import matplotlib.pyplot as plt
-import yfinance 
 
-# Writing to CSV file
-#with open('downData.csv', mode='w', newline='') as file:
-#    writer = csv.writer(file)
-#    writer.writerows(data)
-#print("CSV file populated successfully!")
+def create_graph_for_pulled_data(stock, time):
 
-# | time_input = str(input()) # code to make the csv files named accordingly with respect to the timeframe being downloaded.
+    read = pd.read_csv(f"{stock}_{time}.csv")
 
-# | apple = yfinance.Ticker("AAPL")
-# | data = apple.history(period = time_input)
-# | df_data = pd.DataFrame(data) # convert the data to a dataFrame 
+    pulled_Data_to_Dataframe = pd.DataFrame(read)
 
-# | df_data.to_csv(time_input+".csv" , index = True) # 
+    plt.figure(figsize=(10, 6))
 
-# | print(data.head())
+    plt.plot(pulled_Data_to_Dataframe['Date'], pulled_Data_to_Dataframe['Close'], linestyle='-', color='DarkSlateGrey', label = 
+                                                                                f'{stock.upper()} price during the past {time}')
 
-
-
-# temp code
-def create_graph(t): # going to add s as a functional argument in order to control the stock being studied as well
-
-    # 6 stocks which will be used in graphing process
-    # yfinance data: Apple
-    apple = yfinance.Ticker("AAPL")
-    data = apple.history(period = str(t))
-
-    # yfinance data: Meta
-    meta = yfinance.Ticker("META")
-    data = meta.history(period = srt(t))
     
-    # yfinance data: Alphabet
-    aplphabet = yfinance.Ticker("GOOG")
-    data = alphabet.hisotry(period = srt(t))
-    
-    # yfinance data: Microsoft
-    microsoft = yfinance.Ticker("MSFT")
-    data = microsoft.history(period = srt(t))
-    
-    # yfinance data: Nvidia
-    nvidia = yfinance.Ticker("NVDA")
-    data = nvidia.history(period = srt(t))
-    
-    # yfinance data: Berkshire Hathaway
-    bhath = yfinance.Ticker("BRK.B")
-    data = bhath.history(period = srt(t))
-    
-    
-    df_data = pd.DataFrame(data) # convert the data to a dataFrame 
+    plt.xlabel('Date')
+    plt.ylabel('Closing price ($)')
 
-    return (df_data.to_csv(str(t)+".csv" , index = True))
+    plt.legend()
+    return plt.show()
